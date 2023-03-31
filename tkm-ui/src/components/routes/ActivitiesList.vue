870bf418f208e4bb4e8fa33e597c163a731259eb
Lindsay <lindsay.dev33@gmail.com>
2023-03-31 22:32:18 +0200

    link back and front for activities

--- tkm-ui/src/components/routes/ActivitiesList.vue
+++ tkm-ui/src/components/routes/ActivitiesList.vue
@@ -1,3 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            activities: []
+        }
+    },
+    methods: {
+        async initActivities() {
+            const response = await this.$axios.get('/activities');
+            this.activities = response.data;
+        }
+    },
+    beforeMount() {
+        this.initActivities();
+    }
+};
+</script>
+
 <template>
     <h1 class="text-center m-4">Les activités et restaurants proposés</h1>
 
@@ -8,28 +27,36 @@
                 </div>
             </section>
 
+            <div v-for="activity in activities">
+                Test : {{ activity.name }}
+            </div>
                 
-<section class="row justify-content-center fw-semibold text-center m-5">
+            <section class="row ">
 
-                <div class="col-md-6">
-
+                <div class="col-md-4 d-flex justify-content-center fw-semibold text-center" v-for="activity in activities">
+                    <!-- TEST <img src="src/assets/images/accueilImage/forest.jpg">  -->
                     <div class="card m-1 fw-normal">
-                        <img src="src/assets/images/accueilImage/forest.jpg"
-                    >
-                        <div class="card-body my-1">
-                            <h2>Nom</h2>
-                            <p class="card-text">Description</p>
+                        <img src="src/assets/images/accueilImage/forest.jpg">   
+                        <!--VOIR COMMENT CHARGER IMAGES-->               
+                            <!-- <img :src="activity.imageUrl" :alt="sticker.name">                    -->
+                            
+                            <div class="card-body my-1">
+                                <h2>{{ activity.name }}</h2>
 
-                        <div>
-                            <p>Lieu</p>
-                        </div>
+                                <p class="card-text">{{ activity.description}}</p>
 
-                        <div>
-                            <p><a href="" target="_blank" class="link-title">Lien</a></p>
-                        </div>
+                                <div>
+                                    <p>{{ activity.location}}</p>
+                                </div>
 
-                            <a href=""><img src="src/assets/images/heart.svg"></a>
-                        </div>
+                                <div>
+                                    <p class="fst-italic text-uppercase"> 
+                                        <a v-bind:href="activity.linkUrl" target="_blank" class="link-title">Accès au site</a>
+                                    </p>
+                                </div>
+
+                                <a href=""><img src="src/assets/images/heart.svg"></a>
+                            </div>
                         
                     </div>
 
@@ -37,7 +64,7 @@
             
             </section>
                 <div class="row">
-                    <div class="d-flex justify-content-end">
+                    <div class="d-flex justify-content-end m-3">
                         <button class="btnColor" id="scrollUp">
                             <a href="#top"><img class="btn" src="src/assets/images/arrow-up.svg"/></a>
                         </button>
