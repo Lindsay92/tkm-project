149722dd7e1d5986d686bd14c266f3840db14e11
Lindsay <lindsay.dev33@gmail.com>
2024-06-19 10:03:05 +0200

    refactor dml for new test build with jenkins and update gitignore

--- tkm-db/data.dml.sql
+++ tkm-db/data.dml.sql
@@ -21,14 +21,15 @@ INSERT INTO accounts
 INSERT INTO activities
 	(name, description, image_url, location, link_url, created_at)
 	VALUES
-	('Musee Alexandre Franconie', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 
+	('Musee Alexandre Franconie', 'Le Musée Territorial, inauguré en 1901 sous le nom de Musée Local, est l’une des plus anciennes institutions culturelles de Guyane.', 
 		'flower2.jpg', 'Cayenne', 'https://musee.ctguyane.fr/ws/collections/app/report/index.html', now()
 		),
-	('Tigdilo Marais de Kaw', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 
+	('Tigdilo Marais de Kaw', 'TIGDILO vous propose des circuits de découverte accompagnés de guides Naturalistes passionnés et spécialisés.
+		Durant un circuit unique en pirogue, au plus prêt de la nature, observons ensemble la faune et la flore peuplant cet environnement.', 
 		'forest.jpg', 'Route de Macouria', 'https://tigdilo.fr/', now()
 		),
-	('Centre spatial guyanais', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 
-		'house.jpg', 'Route de Macouria', 'https://centrespatialguyanais.cnes.fr/fr/', now()
+	('Centre spatial guyanais', 'Inauguré en 1996, le musée de l’Espace du Centre spatial guyanais est devenu au fil de ses 26 années d’existence l’un des sites les plus visités de Guyane, et un outil incontournable dans le paysage pédagogique, culturel et touristique de notre région.', 
+		'house.jpg', 'Route de Kourou', 'https://centrespatialguyanais.cnes.fr/fr/le-musee-fait-peau-neuve', now()
 		);
 		
 INSERT INTO likes
